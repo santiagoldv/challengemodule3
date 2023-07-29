3c97c0f6a124be91e8090c9d071304287594d79c
Santiago Ley <santiagoleydelvalle@hotmail.com>
2023-07-29 10:58:08 -0600

    format

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,10 @@ import csv
 electiondata_csv = os.path.join("resources", "election_data.csv")
 
 totalvotes = 0
-candidates = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
-
+candidates =[]
+candidate_votes = {}
+winner = ("")
+vote_count = 0
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -15,36 +17,30 @@ with open(electiondata_csv) as election_file:
 
     for each_line in csv_reader:
         totalvotes +=1
-
-
-
-        """ votes1 = each_line[2]
-        if votes1 is candidates:
-            votes1 = 
-        
-        "for each_row in csv_reader: """
+        candidate =  each_line[2]
+        if candidate not in candidates:
+            candidates.append(candidate)
+            candidate_votes[candidate] = 0
+    
+        candidate_votes[candidate] +=1
 
 print("Election Results")
 print("")
 print("Total votes:", totalvotes)
 print("")
-print("Charles Casper Stockham:", )
-print("Diana DeGette:", )
-print("Raymon Anthony Doane", )
-print("")
-print("Winner:", )
 
-"""
-        current_month = each_line[0]
-        current_profit = int(each_line[1])
-        nettotal += current_profit
-        profit_list += [current_profit]
-        
-        if prev_profit is not None:
-            current_change = current_profit - prev_profit        
-            change_total += current_change
-          
-            if current_change > max_inc_value:
-                max_inc_month = current_month               
-                max_inc_value = current_change 
-"""
+with open(electiondata_csv) as election_file:
+    csv_reader =  csv.reader(election_file, delimiter= ',')
+    next(csv_reader, None)
+
+    for candidate in candidate_votes:
+        votes = candidate_votes.get(candidate)
+        percentaje = float(votes)/float(totalvotes)*100
+        if votes > vote_count:
+            vote_count = votes
+            winner = candidate
+
+        print(candidate, "{:,.2f}".format(percentaje), "%", "{:,.0f}".format(votes))
+print("")
+print("Winner:", winner)
+
